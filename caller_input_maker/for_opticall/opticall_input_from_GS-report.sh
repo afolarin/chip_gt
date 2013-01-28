122640671526cd5988119161b797f69b7a4f86c3
Amos Folarin <afolarinbrc@bignode.cluster.local>
2013-01-28 18:15:39 +0000

    new script for making Opticall input files from ZCall style Genome Studio Report files

---
+++ caller_input_maker/for_opticall/opticall_input_from_GS-report.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+## Builds opticall input from a Genome Studio Report file with the format
+## Name<tab>Chr<tab>Position<tab>Sample1.GType<tab>Sample1.X<tab>Sample1.Y
+## File format output - [SNP] [Coor] [Alleles] [SampleA [intX] [intY]] [SampleN [intX] [intY]]
+
+## USAGE: bash ./opticall_input_from_GS-report.sh <genomeStudioReport>
+
+
+#PARAMS:
+gsReport=${1}
+outputFile=${1}"_opticall"
+
+# Alleles column is essentially a constant AB, as intX == A and intY == B
+
+# Header
+perl -lane '$,="\t"; @intensities=(); for($i=3; $i<=$#F; $i++){ if($i%3 != 0) {push(@intensities, $i);}} if($. == 0) { print("SNP",  "Coor",  "Alleles",  "@F[@intensities]");}' ${gsReport} > ${outputFile}
+
+# Build the file, dropping Chr and Genotype cols.. adding the Allele column for 2nd row to end-of-file
+perl -lane '$,="\t"; @intensities=(); for($i=3; $i<=$#F; $i++){ if($i%3 != 0) {push(@intensities, $i);}} if($. != 0) { print($F[0],  $F[2],  "AB",  "@F[@intensities]");}' ${gsReport} > ${outputFile}
+
